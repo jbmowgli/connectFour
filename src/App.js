af274212d17ebdbe0139996d0787b801a8650d71
John Toddy <john.toddy@manheim.com>
2017-06-18 15:21:05 -0400

    adding react and webpack for visual components

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Board } from 'SRC/components/board';
+
+function App() {
+  return (
+    <div>
+      <Board />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/board.js
@@ -0,0 +1,14 @@
+import React from 'react';
+const Board = ( { boardData } ) => {
+  return (
+    <div>
+      <table>
+        <tr>
+          <td/>
+        </tr>
+      </table>
+    </div>
+  )
+}
+
+export { Board };
